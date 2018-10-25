36581bf2b39b13936a51988a9e3893f51eba8ee3
jfqd <jfqd@blun.org>
2018-10-25 18:28:44 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# haproxy-api
+
+Web-App to enable or disable haproxy backends.
+
+## Installation on OPNSense
+
+Install ruby and dependencies:
+
+```
+pkg install ruby
+opnsense-code tools ports
+cd /usr/ports/devel/ruby-gems && make install
+echo "gem: --no-document" >> /root/.gemrc
+gem install bundler
+```
+
+Install the application:
+
+```
+cd /usr/local/var
+git clone https://github.com/jfqd/haproxy-api.git
+cd haproxy-api
+bundle
+```
+
+Create an `APP_TOKEN` in the `.env` file:
+
+```
+ruby -e 'require "digest/sha1";puts "APP_TOKEN=#{Digest::SHA2.hexdigest((Random.rand*10000000000000000).to_s)}";' > .env
+```
+
+And start the application:
+
+```
+su www -c 'cd /usr/local/var/haproxy-api; rackup -p 9292 -D'
+```
+
+## Usage
+
+tbd.
+
+Copyright (c) 2018 Stefan Husch, qutic development.
---
+++ app.rb
@@ -0,0 +1,95 @@
+# encoding: UTF-8
+require 'rubygems'
+require 'sinatra'
+require 'dotenv/load'
+require 'haproxy_manager'
+
+PLAIN_TEXT = {'Content-Type' => 'text/plain'}
+
+configure do
+  # http://recipes.sinatrarb.com/p/middleware/rack_commonlogger
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
+class String
+  def blank?
+    self == nil || self == ''
+  end
+end
+
+class NilClass
+  def blank?
+    self == nil
+  end
+end
+
+def haproxy
+  @haproxy ||= HAProxyManager::Instance.new(ENV['HAPROXY_SOCKET'] || '/var/run/haproxy.socket')
+end
+
+post '/disable' do
+  begin
+    # authorized user?
+    if params[:token].blank? || params[:token] != ENV['APP_TOKEN']
+      halt 403, PLAIN_TEXT, "unauthorized\n"
+    end
+    # url parameter valid?
+    if params[:backend].blank? || params[:server].blank?
+      halt 422, PLAIN_TEXT, "params missing or unvalid\n"
+    end
+    
+    haproxy.disable(params[:server], params[:backend])
+    halt 200, PLAIN_TEXT, "backend of server was disabled\n"
+    
+  rescue Exception => e
+    logger.warn "[haproxy-api Rescue: #{e.message}"
+    halt 400, PLAIN_TEXT, e.message
+  end
+end
+
+post '/enable' do
+  begin
+    # authorized user?
+    if params[:token].blank? || params[:token] != ENV['APP_TOKEN']
+      halt 403, PLAIN_TEXT, "unauthorized\n"
+    end
+    # url parameter valid?
+    if params[:backend].blank? || params[:server].blank?
+      halt 422, PLAIN_TEXT, "params missing or unvalid\n"
+    end
+    
+    haproxy.enable(params[:backend], params[:server])
+    halt 200, PLAIN_TEXT, "backend of server was enables\n"
+    
+  rescue Exception => e
+    logger.warn "[haproxy-api Rescue: #{e.message}"
+    halt 400, PLAIN_TEXT, e.message
+  end
+end
+
+post '/status' do
+  begin
+    # authorized user?
+    if params[:token].blank? || params[:token] != ENV['APP_TOKEN']
+      halt 403, PLAIN_TEXT, "unauthorized\n"
+    end
+    # url parameter valid?
+    if params[:backend].blank? || params[:server].blank?
+      halt 422, PLAIN_TEXT, "params missing or unvalid\n"
+    end
+    
+    status = haproxy.stats[params[:backend]][params[:server]]["status"]
+    
+    halt 200, PLAIN_TEXT, status
+    
+  rescue Exception => e
+    logger.warn "[haproxy-api Rescue: #{e.message}"
+    halt 400, PLAIN_TEXT, e.message
+  end
+end
+
+get '/ping' do
+  'pong'
+end
